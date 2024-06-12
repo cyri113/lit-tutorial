cddd30296ccd87254eab5099defc53a31148a587
Cyrille Derche <c.derche@me.com>
2024-06-12 17:12:40 +0200

    add evm conditions example

--- vite/src/Encrypt.tsx
+++ vite/src/Encrypt.tsx
@@ -1,8 +1,12 @@
 import { useForm } from "react-hook-form";
 import * as LitJsSdk from "@lit-protocol/lit-node-client";
-import { AccessControlConditions } from "@lit-protocol/types";
+import { AccessControlConditions, EvmContractConditions } from "@lit-protocol/types";
 import { useAccount } from "wagmi";
 import useLit from "./hooks/LitProvider";
+
+// sepolia
+const permissionContractAddress = "0x159c58561685F83C09c76FeD3d85C6eca68d8227"
+const resourceAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 
 interface IEncrypt {
   setParsedJsonData: any
@@ -18,20 +22,62 @@ function Encrypt({ setParsedJsonData, chain }: IEncrypt) {
     await encrypt(data)
   }
 
+  function generateEvmContractConditions(): EvmContractConditions {
+    return [
+      {
+        contractAddress: permissionContractAddress,
+        functionName: "getPermission",
+        functionParams: [resourceAddress, ":userAddress"],
+        functionAbi: {
+          "inputs": [
+            {
+              "internalType": "address",
+              "name": "resource_",
+              "type": "address"
+            },
+            {
+              "internalType": "address",
+              "name": "account_",
+              "type": "address"
+            }
+          ],
+          "name": "getPermission",
+          "outputs": [
+            {
+              "internalType": "bool",
+              "name": "",
+              "type": "bool"
+            }
+          ],
+          "stateMutability": "view",
+          "type": "function"
+        },
+        chain,
+        returnValueTest: {
+          key: "",
+          comparator: '=',
+          value: "true"
+        },
+      }
+    ]
+  }
+
   function generateAccessControlConditions(): AccessControlConditions {
-    return [{
-      contractAddress: "",
-      standardContractType: "",
-      chain,
-      method: "",
-      parameters: [
-        ':userAddress',
-      ],
-      returnValueTest: {
-        comparator: '=',
-        value: address
+    return [
+      {
+        contractAddress: "",
+        standardContractType: "",
+        chain,
+        method: "",
+        parameters: [
+          ':userAddress',
+        ],
+        returnValueTest: {
+          comparator: '=',
+          value: address
+        },
       },
-    }]
+    ]
   }
 
   async function encrypt(dataToEncrypt: any): Promise<void> {
@@ -41,13 +87,17 @@ function Encrypt({ setParsedJsonData, chain }: IEncrypt) {
     }
 
     const accessControlConditions = generateAccessControlConditions()
+    const evmContractConditions = generateEvmContractConditions()
 
     if (litConnected) {
       try {
         console.log('Encrypting...')
         const encryptResponse = await LitJsSdk.encryptToJson({
           string: JSON.stringify(dataToEncrypt),
-          accessControlConditions, litNodeClient, chain,
+          // accessControlConditions, 
+          evmContractConditions,
+          litNodeClient,
+          chain,
         })
         console.log('Encrypted :)')
         const json = JSON.parse(encryptResponse)
